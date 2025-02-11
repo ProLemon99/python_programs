6801bb9966b5c91e2e097a20732ed5954f69833f
Pixel Lemons <levin.shao@education.nsw.gov.au>
2025-02-11 20:51:41 +1100

    some minor updates

--- games/simple/rps.py
+++ games/simple/rps.py
@@ -1,26 +1,31 @@
 import random
 
 def play():
-    user = input("What's your choice? 'r' for rock, 'p' for paper, 's' for scissors\n")
-    computer = random.choice(['r', 'p', 's'])
+    while True:
+        user = input("What's your choice? 'r' for rock, 'p' for paper, 's' for scissors, or 'q' to quit\n")
+        if user == 'q':
+            print("Thanks for playing!")
+            break
+        
+        if user not in ['r', 'p', 's']:
+            print("Invalid choice. Please choose 'r', 'p', 's', or 'q' to quit.")
+            continue
+        
+        computer = random.choice(['r', 'p', 's'])
+        print(f"Computer chose: {computer}")
 
-    if user == computer:
-        print(computer)
-        return 'It\'s a tie'
-
-    # r > s, s > p, p > r
-    if is_win(user, computer):
-        print(computer)
-        return 'You won!'
-
-    print(computer)
-    return 'You lost!'
+        if user == computer:
+            print("It's a tie!")
+        elif is_win(user, computer):
+            print("You won!")
+        else:
+            print("You lost!")
 
 def is_win(player, opponent):
-    # return true if player wins
+    # return True if player wins
     # r > s, s > p, p > r
-    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') \
-        or (player == 'p' and opponent == 'r'):
-        return True
+    return (player == 'r' and opponent == 's') or \
+           (player == 's' and opponent == 'p') or \
+           (player == 'p' and opponent == 'r')
 
-print(play())
+play()
--- tools/password-generator.py
+++ tools/password-generator.py
@@ -3,9 +3,29 @@ import string
 
 def generate_password(length):
     characters = string.ascii_letters + string.digits + string.punctuation
-    password = ''.join(random.choice(characters) for i in range(length))
+    password = ''.join(random.choice(characters) for _ in range(length))
     return password
 
-print("Hello, this is a random password generator that will generate a secure password made of a combination of letters, numbers and symbols.")
-length = int(input("Enter the length of the password: "))
-print("Your password is:", generate_password(length))
+print("Hello, this is a random password generator that will generate a secure password made of a combination of letters, numbers, and symbols.")
+
+while True:
+    try:
+        length = int(input("Enter the length of the password (max 30): "))
+        if length <= 0:
+            print("Please enter a positive number.")
+            continue
+        elif length > 30:
+            print("Don't you think the password would be... too secure?")
+            continue
+    except ValueError:
+        print("Invalid input! Please enter a valid number.")
+        continue
+
+    while True:
+        password = generate_password(length)
+        print("Your password is:", password)
+
+        reroll = input("Do you want to generate another password? (Y/N): ").strip().lower()
+        if reroll != 'y':
+            print("Goodbye! Stay secure.")
+            exit()
